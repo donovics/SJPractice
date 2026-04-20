281e378835aaae4c32b832d9f0d33178056eef8a
donovics <kerchnerdani@gamil.com>
2026-04-20 18:40:41 +0200

    SQLite database

--- Main/RESTing.js
+++ Main/RESTing.js
@@ -1,26 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const database = require("better-sqlite3");
+const db = new database("shelf.db")
 
 app.use(express.json());
 
-let people = [
-    { id: 1, name: "Peter", age: 22, num: 16},
-    { id: 2, name: "Scarlet", age: 21, num: 4},
-    { id: 3, name: "John", age: 222, num: 0}
-];
+db.exec(`
+    CREATE TABLE IF NOT EXISTS people (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    num INTEGER
+    )
+    `);
 
-app.get("/data", (req, res) => { res.status(200).json(people); console.log("People listed."); });
+app.get("/data", (req, res) => { 
+    let people = db.prepare("SELECT * FROM people").all();
+    res.status(200).json(people);
+    console.log("People listed.");
+});
 
-app.post("/data", (req, res) => { let newPerson = 
-{
-    id: req.body.id,
-    name: req.body.name,
-    age: req.body.age,
-    num: req.body.num
-};
-    people.push(newPerson);
-    res.status(201).json();
-    console.log("Person added.");
+app.post("/data", (req, res) => {
+    try{
+        // const { id, name, age, num } = req.body;
+        let result = db.prepare("INSERT INTO people (id, name, age, num) VALUES (?, ?, ?, ?)")
+            .run(req.body.id, req.body.name, req.body.age, req.body.num);
+        res.status(201).json();
+        console.log("Person added.");
+    }
+    catch (err){
+        res.status(400).json({ error: err.message });
+    }
 });
 
 app.listen(8080, () => {
