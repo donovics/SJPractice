8834642c535bd9223586cd695bc789f7c57b96e0
donovics <kerchnerdani@gamil.com>
2026-04-24 14:21:27 +0200

    React done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { POSTEndpoint } from "./Endpoints.js"
+import { GETEndpoint } from "./Endpoints.js"
+import { DELETEEndpoint } from "./Endpoints.js"
 
 function App() {
   return (
@@ -12,8 +14,16 @@ function App() {
       <input type="number" id="age" name="age"></input><br></br>
       <label>num: </label>
       <input type="number" id="num" name="num"></input><br></br>
-      <button onClick={ POSTEndpoint }>Send</button><br></br><br></br>
-      <script type="module" src="Endpoints.js"></script>
+      <button onClick={ POSTEndpoint }>Send</button><br></br>
+      <br></br>
+      <h3>List people from database:</h3>
+      <button onClick={ GETEndpoint } >List people</button><br></br>
+      <span id="textarea"></span>
+      <h3>Delete person from database:</h3>
+      <label>The id of the person: </label>
+      <input type="number" id="delid" name="delid"></input><br></br>
+      <button onClick={ DELETEEndpoint } >Delete</button><br></br>
+
     </div>
   );
 }
--- src/Endpoints.js
+++ src/Endpoints.js
@@ -5,11 +5,6 @@ export function POSTEndpoint(){
     let num = document.getElementById("num").value;
 
     let xhttp = new XMLHttpRequest();
-    /*xhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-             alert(this.responseText);
-         }
-    };*/
     xhttp.open("POST", "http://localhost:8080/data", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send('{ "id":' + id + ', ' +
@@ -17,4 +12,23 @@ export function POSTEndpoint(){
         '"age": ' + age + ', ' +
         '"num": ' + num +
         '}');
+}
+
+export function GETEndpoint(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("textarea").innerHTML = this.responseText;
+         }
+    };
+    xhttp.open("GET", "http://localhost:8080/data", true);
+    xhttp.send();
+}
+
+export function DELETEEndpoint(){
+    let id = document.getElementById("delid").value;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "http://localhost:8080/data/" + id, true);
+    xhttp.send();
 }
